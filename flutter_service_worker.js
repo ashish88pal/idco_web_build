986462a7300ecbbe3b5a34b1a9992dcf6bfc2f9a
Ashish Pal <ashish.hack.dev@gmail.com>
2023-07-24 08:20:33 +0530

    idco web build init

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,8 +23,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "8d3e39cee06b476dd3fcd77186009cca",
-"/": "8d3e39cee06b476dd3fcd77186009cca",
+"index.html": "d96314f8d27b94777effb0c41756ac55",
+"/": "d96314f8d27b94777effb0c41756ac55",
 "main.dart.js": "2da6b51e1d734209c7243f1998cb300e",
 "manifest.json": "95275fee483305a4fb0a71fa3024760a",
 "version.json": "4f023eac5a6b6395b4947ce5b1fb63ac"};
