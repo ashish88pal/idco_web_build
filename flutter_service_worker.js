20d53230c39ad6b38d788f67b02fee7e6b8545db
ashish88pal <ashish.hack.dev@gmail.com>
2024-02-09 12:36:08 +0530

    manage member login

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,9 +5,9 @@ const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "4f023eac5a6b6395b4947ce5b1fb63ac",
 "favicon.ico": "717a6d5f75e39b1f8689d3d86b27c3c3",
-"index.html": "e20098737b59149ced52308b408f0136",
+"index.html": "668dc82009ae11d5234b7e5eba417dcc",
 "/": "6610107c2ae1ec1a2e1afe3c0209e3a1",
-"main.dart.js": "f0701fc5e373b124b54d5c29b3915173",
+"main.dart.js": "77d036457117cbc2935ed23ebf0d5b7e",
 "flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "main-icon.png": "b4ff80cb5c12f2812ed248e179630587",
