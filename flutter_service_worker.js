fc4bb1d6db7991d38bde60f59b6c5b9ec1ee1753
ashish88pal <ashish.hack.dev@gmail.com>
2024-02-13 11:07:15 +0530

    v1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,9 +5,9 @@ const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "4f023eac5a6b6395b4947ce5b1fb63ac",
 "favicon.ico": "717a6d5f75e39b1f8689d3d86b27c3c3",
-"index.html": "e0d79581ac6a34fe22c39c7575b53fea",
+"index.html": "e0af8d1ba211cdd46f8dde230e62cbb5",
 "/": "6610107c2ae1ec1a2e1afe3c0209e3a1",
-"main.dart.js": "8bbdc4cb28f0d83092b7d077b8a55c96",
+"main.dart.js": "eaa034824ac5e936b1a876313e5ec56b",
 "flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "main-icon.png": "b4ff80cb5c12f2812ed248e179630587",
